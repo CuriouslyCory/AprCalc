5efcdbc3fba0f84ba366602691ae913da7bee56d
Cory Sougstad <cory@hau.me>
2021-11-07 09:14:11 -0800

    Adding some things I forgot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'apr-calc';
+  title = 'APR Calculator';
+  apr: number = 10;
+  days: number = 365;
+  starting_capital: number = 10000;
+  results: Array<number> = [];
+
+  constructor() {}
+
+  recalc() {
+    this.results = [];
+    let amount: number = this.starting_capital;
+    for(let i=0; i<this.days; i++){
+      amount = amount * ((this.apr / 365/ 100) + 1);
+      this.results.push(amount);
+    }
+  }
 }
+//=INDIRECT(ADDRESS(ROW()-1, COLUMN()))*((C2/365/100)+1)
